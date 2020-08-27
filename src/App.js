af3a4921fbaa080f56a07b1e47e60924daa114f0
Raymond Oluoch <rasholuoch@gmail.com>
2020-08-28 01:49:11 +0300

    app installation and configuration - milestone one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const H4 = styled.h2`
+  font-weight: bold;
+  line-height: 52px;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <H4>The Bookstore</H4>
       </header>
     </div>
   );
